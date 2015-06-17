1847fe35031189e6800cb61746b7ba838de3290f
JBlohm <joeblohm@gmail.com>
2015-06-17 22:21:38 +0200

    primary task completed

--- run_analysis.R
+++ run_analysis.R
@@ -24,7 +24,7 @@ library(dplyr)
 features_dt <- data.table(read.csv("./UCI HAR Dataset/features.txt", header=FALSE, sep=" ", col.names=c("feature_id", "feature")))
 
 ## Read table of activity labels 
-activity_labels_dt <- data.table(read.csv("./UCI HAR Dataset/activity_labels.txt", header=FALSE, sep=" ", col.names=c("activity_id", "activity")))
+activity_labels_dt <- data.table(read.csv("./UCI HAR Dataset/activity_labels.txt", header=FALSE, sep=" ", col.names=c("activity_class", "activity")))
 
 #######################
 ## Get Training Data ##
@@ -84,8 +84,46 @@ test_set_dt <- mutate(test_set_dt, subject = subject_test_dt)
 ## Combining observations from training & test tables
 #####################################################
 
-## Put test group observations below traing groups
+## Put test group observations below training groups
 data_set <- rbind(training_set_dt, test_set_dt)
+
+#########################################################
+## Extract only the measurements on the mean an std. dev.
+## We also leave the subject and activity_class in here.
+#########################################################
+
+## Generate a list vector containing matching column indices grep(value=FALSE)
+c_v <- grep(".mean.", names(test_set_dt), value=FALSE, fixed=TRUE)
+c_v <- c(c_v, grep(".std.", names(test_set_dt), value=FALSE, fixed=TRUE))
+c_v <- c(c_v, grep("subject", names(test_set_dt), value=FALSE, fixed=TRUE))
+c_v <- c(c_v, grep("activity_class", names(test_set_dt), value=FALSE, fixed=TRUE))
+
+## In dplyr you can subset on that list of indices
+extracted_data_set <- select(data_set, c_v)
+
+###############################################################################
+## Now we have created our data_set with appropriate descriptive variable names
+## We will now replace the activity id's with their descriptive activity names
+###############################################################################
+
+extracted_data_set <- left_join(extracted_data_set, activity_labels_dt, by="activity_class")
+
+## We can remove the activit_class code, because we now have the named activity in the table
+extracted_data_set <- select(extracted_data_set, -activity_class)
+
+################################################################################
+## Create the tidy data set with the average (mean) of each activity per subject
+################################################################################
+## This is a split, apply, combine problem:
+## If not already done: install.packages("plyr")
+library(plyr)
+
+# melting & casting with averaging on activity and subject for all variables, calculating the mean
+tidy_data_Melt <- melt(extracted_data_set,id=c("activity","subject"))
+tidy_data <- dcast(tidy_data_Melt, activity+subject ~ variable, mean)
+
+## Write table to .txt file:
+write.table(tidy_data, "./tidy_data_set.txt", row.names=FALSE)
 
 
 
